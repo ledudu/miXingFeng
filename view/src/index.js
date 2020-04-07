7d1177120aabd20591628b9608e11eabc09a3b8e
zhoushoujian <863165094@qq.com>
2020-04-07 22:07:54 +0800

    weChat Share

--- view/src/index.js
+++ view/src/index.js
@@ -1,148 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import _ from "lodash";
-import axios from 'axios';
-import { Toast } from "antd-mobile";
+import axios from 'axios'
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from 'redux';
-import { createLogger } from 'redux-logger';
 import { Provider } from "react-redux";
-import Logger from "cordova-logger"
 
 import reducer from "./ducks";
-import { updateToken } from "./ducks/login";
 import Routers from './router';
-import { checkToShowPlayController } from "./logic/common"
-import { alertDialog } from "./services/utils"
-import "./services/JPush";
-import "./services/native";
 import './themes/css/index.less';
 
 window.serverHost = window.config.debug ? (window.config.domain + ":" + window.config.port) :  window.config.domainUrl
 window.isDevModel = window.config.dev
-const allowReadAndWriteFile = localStorage.getItem("allowReadAndWriteFile")
-if(window.isCordova){
-	if(allowReadAndWriteFile){
-		window.logger = new Logger({
-			folder: "miXingFeng",
-			column: "log",
-			filename: "miXingFeng.txt"
-		})
-	} else {
-		// 安装后首次打开需要有文件读写权限才能记录日志,没得到权限之前使用console
-		window.logger = console
-	}
-} else {
-	// h5端打印log带上时间
-	window.logger = new Logger()
-}
+window.logger = console
 
-//apply Reducer
 const middleware = [thunk];
-if(window.config && window.isDevModel){
-	middleware.push(createLogger({
-		collapsed: false,
-	}));
-}
 const store = createStore(reducer, compose(applyMiddleware(...middleware)));
 window.store = store;
 window.$dispatch = store.dispatch;
 window.$getState = store.getState;
 window.$subscribe = store.subscribe;
-window._ = _;
 window.axios = axios;
 window.alertOld = window.alert
-window.alert = (text) => {
-	if(window.isCordova){
-		window.plugins.toast.showShortCenter(text)
-	} else {
-		Toast.info(text, 2, null, false);
-	}
-}
-window.alertDebug = (text) => {
-	// don't comment this function content in production
-	if(window.isDevModel){
-		if(window.isCordova){
-			// window.plugins.toast.showShortCenter(text)
-			alertDialog(text)
-		} else {
-			alertDialog(text);
-		}
-	}
-	logger.info('alertDebug', text)
-}
+window.alertDialog = window.alert
 
-window.alertDialog = alertDialog
-
-//change router
-window.goRoute = ( self, path ) => {
-	if(!self && window.SELF) {
-		logger.warn("window.goRoute window.SELF path", path)
-		self = window.SELF;
-	} else if(self){
-
-	} else {
-		logger.warn("window.goRoute", window.SELF)
-		return
-	}
-	logger.info("window.goRoute inner func path", path)
-	checkToShowPlayController()
-	return self.props.history.push(path);
-}
 
 window.getRoute = () => {
 	return window.location.href.replace(/.+#/, "");
 }
 
-axios.interceptors.request.use(function (config) {
-	const { token, userId } = window.$getState().login
-	logger.info("axios.interceptors.request userId", userId)
-    if (token) {
-		config.headers.Authorization = token;
-	}
-	if(config.method === 'get'){
-		if(/[?]/.test(config.url)){
-			config.url = `${config.url}&userId=${userId}`
-		} else {
-			config.url = `${config.url}?userId=${userId}`
-		}
-	}
-	if(config.method === "post"){
-		config.data.userId = userId
-	}
-    return config;
-}, function (err) {
-	logger.error("axios.interceptors.request err", err)
-    return Promise.reject(err);
-})
-axios.interceptors.response.use(
-	response => {
-		return response
-	},
-	error => {
-		if (error.response) {
-			if(!error.response.data) {
-				alertDebug(`error.response ${error.response}`)
-				return logger.error('axios.interceptors.response.use error.response', error.response)
-			} else if(!error.response.data.result){
-				alertDebug(`error.response.data ${error.response.data}`)
-				return logger.error('axios.interceptors.response.use error.response.data', error.response.data)
-			}
-			switch (error.response.data.result.errCode) {
-				case 401:
-					window.$dispatch(updateToken(""));
-					if(!window.isCordova){
-						window.localStorage.removeItem("tk");
-					}
-					logger.warn("axios.interceptors.response.use  token已过期")
-					alertDebug('token已过期')
-				default:
-					alertDebug(`error.response.data.result.errCode: ${error.response.data.result.errCode}`)
-					logger.error(`error.response.data.result.errCode: ${error.response.data.result.errCode}`)
-			}
-		}
-		return Promise.reject(error.response && error.response.data) // 返回接口返回的错误信息
-})
 
 window.addEventListener('rejectionhandled', event => {
 	logger.error('rejectionhandled', event.reason)
--- view/src/router.js
+++ view/src/router.js
@@ -2,168 +2,6 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { HashRouter as Router, Route, Switch  } from 'react-router-dom';
 import MyLoadable from './Loadable';
-import { updateAlwaysShowAdsPage, updateHideNavBar, updateAllowGetPosition, updateAllowOthersGetPosition } from "./ducks/common"
-import { updateDirectShowSignPage } from "./ducks/sign";
-import { checkMusicPlayWays, checkLastMusicPlayInfo, stopMusic, requestPositionPermission } from "./logic/common"
-import { initWebsocket } from "./logic/common" ;
-import { onBackKeyDown, requestFileWritePriority } from "./services/utils"
-
-const Login = MyLoadable({
-    loader: () => import('./components/login')
-});
-const SearchUserHistory = MyLoadable({
-    loader: () => import('./components/searchUserHistory')
-});
-const About = MyLoadable({
-    loader: () => import("./components/about")
-});
-const Feedback = MyLoadable({
-    loader: () => import("./components/feedback")
-});
-const SetNickname = MyLoadable({
-    loader: () => import("./components/updateUserInfo/setNickname")
-});
-const SetMobile = MyLoadable({
-    loader: () => import("./components/updateUserInfo/setMobile")
-});
-const SetSignature = MyLoadable({
-    loader: () => import("./components/updateUserInfo/setSignature"),
-});
-const UserProfile = MyLoadable({
-    loader: () => import("./components/userProfile")
-});
-const SystemSetup = MyLoadable({
-    loader: () => import("./components/systemSetup")
-});
-const Licence = MyLoadable({
-    loader: () => import("./components/about/licence")
-});
-const ServiceList = MyLoadable({
-    loader: () => import("./components/about/serviceList")
-});
-const UserAgreement = MyLoadable({
-    loader: () => import("./components/about/userAgreement")
-});
-const Privacy = MyLoadable({
-    loader: () => import("./components/about/privacy")
-});
-const PackageIframe = MyLoadable({
-    loader: () => import("./components/child/packageIframe")
-});
-const ResetPasswordSys = MyLoadable({
-    loader: () => import("./components/resetPasswordSys")
-})
-const MainFrame = MyLoadable({
-    loader: () => import("./components/index")
-});
-
-const Entry = MyLoadable({
-    loader: () => import("./components/entry")
-});
-
-const LookHeadPic = MyLoadable({
-    loader: () => import("./components/lookHeadPic")
-});
-
-const SearchColumn = MyLoadable({
-    loader: () => import("./components/searchColumn")
-});
-
-const SearchPosition = MyLoadable({
-    loader: () => import("./components/searchPosition")
-});
-
-const ShowOnlinePersons = MyLoadable({
-    loader: () => import("./components/showOnlinePersons")
-});
-
-const MySites = MyLoadable({
-	loader: () => import("./components/mySites")
-})
-
-const TypeShell = MyLoadable({
-	loader: () => import("./components/typeShell")
-})
-
-const LoginRecord = MyLoadable({
-	loader: () => import("./components/loginRecord")
-})
-
-const Notification = MyLoadable({
-	loader: () => import("./components/notification")
-})
-
-const SavedSongs = MyLoadable({
-	loader: () => import("./components/savedSongs")
-})
-
-const MyFinishedFiles = MyLoadable({
-	loader: () => import("./components/myFinishedFiles")
-})
-
-const MyFinishedMusics = MyLoadable({
-	loader: () => import("./components/myFinishedMusics")
-})
-
-const MyGames = MyLoadable({
-	loader: () => import("./components/myGames")
-})
-
-const MusicPlayer = MyLoadable({
-	loader: () => import("./components/musicPlayer")
-})
-
-const MyDownloadMiddlePage = MyLoadable({
-	loader: () => import("./components/myDownloadMiddlePage")
-})
-
-const NicknamePage = MyLoadable({
-	loader: () => import("./components/nicknamePage")
-})
-
-const SetEmail = MyLoadable({
-	loader: () => import("./components/updateUserInfo/setEmail")
-})
-
-const CheckEmail = MyLoadable({
-	loader: () => import("./components/updateUserInfo/checkEmail")
-})
-
-const CheckMobile = MyLoadable({
-	loader: () => import("./components/updateUserInfo/checkMobile")
-})
-
-const ForgetPassword = MyLoadable({
-	loader: () => import("./components/forgetPassword")
-})
-
-const Register = MyLoadable({
-	loader: () => import("./components/register")
-})
-
-const SearchFile = MyLoadable({
-	loader: () => import("./components/searchFile")
-})
-
-const SearchMusic = MyLoadable({
-	loader: () => import("./components/searchMusic")
-})
-
-const SearchOnlineMusic = MyLoadable({
-	loader: () => import("./components/searchOnlineMusic")
-})
-
-const SearchAll = MyLoadable({
-	loader: () => import("./components/searchAll")
-})
-
-const MusicController = MyLoadable({
-	loader: () => import("./components/child/musicController")
-})
-
-const RecentMusicPlayed = MyLoadable({
-	loader: () => import("./components/RecentMusicPlayed")
-})
 
 const MusicMvPlayer = MyLoadable({
 	loader: () => import("./components/musicMvPlayer")
@@ -173,9 +11,6 @@ const MusicPlaying = MyLoadable({
 	loader: () => import("./components/musicPlaying")
 })
 
-const SetUsername = MyLoadable({
-	loader: () => import("./components/updateUserInfo/setUsername")
-})
 
 const ShowFileInfo = MyLoadable({
 	loader: () => import("./components/showFileInfo")
@@ -186,57 +21,7 @@ class Routers extends Component {
     componentDidMount(){
 		document.getElementById("root").classList.remove('loading-text')
 		document.getElementById("root").classList.remove("animate-flicker")
-		// 初始化websocket
-		initWebsocket()
-		// 首次启动不是主页自动返回主页
-		const hash = window.getRoute()
-		if(hash !== "/" && !hash.includes("isFromThirdPart") && !hash.includes("show_file_info?filename=")) window.location.href = window.location.href.split("#/")[0];
-		// 检查是否每次都显示广告
-		if(localStorage.getItem("alwaysShowAdsPage") === "no"){
-			$dispatch(updateAlwaysShowAdsPage(false))
-			$dispatch(updateDirectShowSignPage(true))
-			$dispatch(updateHideNavBar(false))
-		}
-		// 检查是否允许程序获取地理位置
-		if(localStorage.getItem("usePosition") === "no"){
-			$dispatch(updateAllowGetPosition(false))
-		}
-		// 检查是否允许其他人获取自己的地理位置
-		if(localStorage.getItem("allowOthersGetPosition") === "no"){
-			$dispatch(updateAllowOthersGetPosition(false))
-		}
-		// 检查音乐播放方式,比如单曲循环,顺序播放
-		checkMusicPlayWays()
-		// 检查上次播放的最后一首音乐
-		checkLastMusicPlayInfo()
-		// 监听物理返回键
-		document.addEventListener("deviceready", this.listenBackFunc);
-		logger.info("Router.js window.location.href", window.location.href)
-	}
 
-	componentWillUnmount(){
-		logger.info("router.js componentWillUnmount will exit app, window.ws && ws.readyState", window.ws && ws.readyState)
-		if(window.isCordova) cordova.plugins.notification.local.clearAll()
-		if(window.ws && ws.readyState ===1){
-			window.ws.close(1000)
-		}
-		const { soundPlaying } = $getState().fileServer
-		if(soundPlaying) stopMusic()
-		document.removeEventListener("deviceready", this.listenBackFunc);
-		document.removeEventListener("backbutton", onBackKeyDown);
-	}
-
-	listenBackFunc = () => {
-		document.addEventListener("backbutton", onBackKeyDown, false);
-		const allowReadAndWriteFile = localStorage.getItem("allowReadAndWriteFile")
-		const allowGetPosition = localStorage.getItem("usePosition")
-		let requestPermission = Promise.resolve()
-		if(!allowReadAndWriteFile){
-			requestPermission = requestFileWritePriority()
-		}
-		if(!allowGetPosition){
-			requestPermission.then(requestPositionPermission)
-		}
 	}
 
     render(){
@@ -245,55 +30,10 @@ class Routers extends Component {
     			<div className="container">
 					<div className="main-content">
     					<Switch>
-                    	    <Route path="/register" component={Register} />
-    					    <Route path="/forget_password" component={ForgetPassword} />
-                    	    <Route path="/login" component={Login} />
-    					    <Route path="/search_user_history" component={SearchUserHistory} />
-                    	    <Route path="/main" component={MainFrame} />
-                    	    <Route path="/about" component={About} />
-                    	    <Route path="/feedback" component={Feedback} />
-                    	    <Route path="/set_nickname" component={SetNickname} />
-                    	    <Route path="/set_mobile" component={SetMobile} />
-                    	    <Route path="/set_signature" component={SetSignature} />
-                    	    <Route path="/user_profile" component={UserProfile} />
-                    	    <Route path="/system_setup" component={SystemSetup} />
-                    	    <Route path="/licence" component={Licence} />
-                    	    <Route path="/service_list" component={ServiceList} />
-                    	    <Route path="/user_agreement" component={UserAgreement} />
-                    	    <Route path="/privacy" component={Privacy} />
-                    	    <Route path="/package_iframe" component={PackageIframe} />
-                    	    <Route path="/reset_password_sys" component={ResetPasswordSys} />
-							<Route path="/look_head_pic" component={LookHeadPic} />
-							<Route path="/search_column" component={SearchColumn} />
-							<Route path="/search_position" component={SearchPosition} />
-							<Route path="/show_online_persons" component={ShowOnlinePersons} />
-							<Route path="/my_sites" component={MySites} />
-							<Route path="/type_shell" component={TypeShell} />
-							<Route path="/login_record" component={LoginRecord} />
-							<Route path="/notification" component={Notification} />
-							<Route path="/saved_songs" component={SavedSongs} />
-							<Route path="/my_finished_files" component={MyFinishedFiles} />
-							<Route path="/my_finished_musics" component={MyFinishedMusics} />
-							<Route path="/my_games" component={MyGames} />
-							<Route path="/my_download_middle_page" component={MyDownloadMiddlePage} />
-							<Route path="/nickname_page" component={NicknamePage} />
-							<Route path="/set_email" component={SetEmail} />
-							<Route path="/check_email" component={CheckEmail} />
-							<Route path="/check_mobile" component={CheckMobile} />
-							<Route path="/search_file" component={SearchFile} />
-							<Route path="/search_music" component={SearchMusic} />
-							<Route path="/search_online_music" component={SearchOnlineMusic} />
-							<Route path="/search_all" component={SearchAll} />
-							<Route path="/recent_music_played" component={RecentMusicPlayed} />
 							<Route path="/music_mv_player" component={MusicMvPlayer} />
 							<Route path="/music_playing" component={MusicPlaying} />
-							<Route path="/set_username" component={SetUsername} />
 							<Route path="/show_file_info" component={ShowFileInfo} />
-                    	    <Route path="/" exact component={Entry} />
     					</Switch>
-					</div>
-					<div className="window-music-container">
-						<MusicController />
 					</div>
     			</div>
             </Router>
